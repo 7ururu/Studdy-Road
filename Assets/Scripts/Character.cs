a5f41595c804b48c6bd99da04ef0f2aa19ef8cf1
Koblyk Dmitry <koblyk@panzerdog.com>
2017-08-23 00:29:28 +0200

    Initial commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Character : MonoBehaviour {
+
+    private enum Direction {
+
+        Left = 0,
+        Forward = 1,
+        Right = 2,
+    }
+
+    [Header("Animator")]
+    [SerializeField]
+    private string _idleAnimationName;
+
+    [SerializeField]
+    private string _moveForwardAnimationName;
+
+    [SerializeField]
+    private string _moveLeftAnimationName;
+
+    [SerializeField]
+    private string _moveRightAnimationName;
+
+    [SerializeField]
+    private string _moveBackAnimationName;
+
+    private Animator _animator;
+    private Direction _direction = Direction.Forward;
+
+
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (_animator.GetCurrentAnimatorStateInfo(0).shortNameHash ==
+            Animator.StringToHash(_idleAnimationName)) {
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
+                Move(Direction.Forward);
+            } else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
+                Move(Direction.Left);
+            } else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
+                Move(Direction.Right);
+            }
+        }
+    }
+
+    private void Move(Direction direction) {
+        switch (_direction - direction) {
+            case 0:
+                _animator.Play(_moveForwardAnimationName);
+                break;
+            case -1:
+                _animator.Play(_moveRightAnimationName);
+                break;
+            case 1:
+                _animator.Play(_moveLeftAnimationName);
+                break;
+            case -2:
+            case 2:
+                _animator.Play(_moveBackAnimationName);
+                break;
+        }
+        _direction = direction;
+    }
+}
---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameCamera : MonoBehaviour {
+
+    [SerializeField]
+    private Character _character;
+
+    [SerializeField]
+    private float _smoothTime = 0.1f;
+
+    [SerializeField]
+    private float _maxSpeed = 1f;
+
+    private Vector3 _velocity;
+
+
+    private void LateUpdate() {
+        var p = new Vector3(_character.transform.position.x, transform.position.y, 
+                            _character.transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, p, ref _velocity, 
+                                                _smoothTime, _maxSpeed, Time.deltaTime);
+    }
+}
